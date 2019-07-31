2afbee05b8f9e6a4261729c99b667e3875345852
mcebisi <mcebisimkhohliwe@gmail.com>
2019-07-31 20:15:21 +0200

    Validation template

---
+++ NiceArray.Library/IValidators/IValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NiceArray_Library.IValidators
+{
+    public interface IValidator
+    {
+        List<string> Validate ( List<string> list );
+    }
+}
