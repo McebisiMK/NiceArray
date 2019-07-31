eb3f30741f6f891eef06d92cae8780d2817e3a18
mcebisi <mcebisimkhohliwe@gmail.com>
2019-07-31 20:12:36 +0200

    Handling invalid input using custom exception

---
+++ NiceArray.Library/Exceptions/InvalidNumberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NiceArray_Library.Exceptions
+{
+    public class InvalidNumberException:Exception
+    {
+        static string message = "can not be converted to numbers";
+
+        public InvalidNumberException (List<string> invalids):base($" '{string.Join(", ", invalids)}' {message}")
+        {
+        }
+    }
+}
